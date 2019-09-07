9503bda7b61f56b727ab93c42f34430b43f1a1ce
Adam Rush <adamrushuk@gmail.com>
2019-09-07 08:13:57 +0100

    Added comments

--- 2_vault_and_modules/commands.sh
+++ 2_vault_and_modules/commands.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-#vault options
+# Vault options
+# Encrypt full vars file
 ansible-vault encrypt group_vars/all.yml --ask-vault-pass
+# Decrypt full vars file
 ansible-vault decrypt group_vars/all.yml --ask-vault-pass
+
+# Encrypt a single var named 'ansible_password', with value 'vagrant'
 ansible-vault encrypt_string --ask-vault-pass 'vagrant' --name 'ansible_password'
+
+# Standard command no longer works
 ansible-playbook playbook.yml
+
+# '--ask-vault-pass' switch must be used to prompt for vault password
+# '--step' is used to prompt user at every step
 ansible-playbook playbook.yml --ask-vault-pass --step
