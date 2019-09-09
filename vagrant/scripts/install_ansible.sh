949cc74b8d25c1a6bac2eb25b40352e95be82196
Adam Rush <adamrushuk@gmail.com>
2019-09-09 16:43:48 +0100

    Added notes and comments on virtual env activation

--- vagrant/scripts/install_ansible.sh
+++ vagrant/scripts/install_ansible.sh
@@ -13,5 +13,10 @@ pip -V
 
 # Install Ansible and pywinrm (for Windows support)
 pip install --upgrade pip setuptools ansible[azure]==2.8.4 pywinrm
-echo "INFO: Finished Installing Ansible."
-echo "INFO: To activate virtual environment, run 'source ~/python-env/ansible2.8.4/bin/activate'"
+echo ""
+echo "INFO: Finished Installing Ansible"
+echo "####################################################################################"
+echo "To activate virtual environment, run the following command:"
+echo "source ~/python-env/ansible2.8.4/bin/activate"
+echo "####################################################################################"
+echo ""
